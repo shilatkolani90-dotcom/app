8d51c3672a3036c7fee7b98011e77bf1f66c8468
Gaia-dover <gaia.dover@gmail.com>
2026-09-15 14:25:05 +0300

    new

---
+++ openscreen.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import *
+
+from numpy.ma.core import size
+
+window=Tk()
+window.geometry("820x820")
+window.title("my first app")
+window.config(background="#ebaeb8")
+photo=PhotoImage(file='mom.png')
+
+#relief=RAISED,bd=10,padx=20,pady=20,image=photo,compound="top"
+label=Label(window,text="hello,welcome to our site",font=('Flux',40,'bold'),fg="white",background="#ebaeb8")
+label.place(x=70,y=0)
+window.mainloop()
