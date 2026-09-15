630b9b62821fb7d5bc04a83f2b45e0e9ac4cc319
Gaia-dover <gaia.dover@gmail.com>
2026-09-15 12:38:20 +0300

    new

--- try.py
+++ try.py
@@ -1,8 +1,12 @@
 import tkinter
 from tkinter import *
 
+photo=PhotoImage(file='mom.png')
 window=Tk()
 window.geometry("820x820")
 window.title("my first app")
-
+window.config(background="#ebaeb8")
+#relief=RAISED,bd=10,padx=20,pady=20
+label=Label(window,text="hello",font=('Flux',40,'bold'),fg="green",image=photo)
+label.place(x=350,y=350)
 window.mainloop()
