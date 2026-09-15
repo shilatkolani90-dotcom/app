d824ab513921c5311b33be848ef399f06642797c
arbelz08 <arbelz08@gmail.com>
2026-09-15 16:35:42 +0300

    arbel

--- openscreen.py
+++ openscreen.py
@@ -1,7 +1,8 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 
-from numpy.ma.core import size
+# from numpy.ma.core import size
 #shalom
 window=Tk()
 window.geometry("820x820")
@@ -17,4 +18,20 @@ button.config(compound=click())
 # relief=RAISED,bd=10,padx=20,pady=20,image=photo,compound="top"
 label=Label(window,text="hello,welcome to our site",font=('Flux',40,'bold'),fg="white",background="#ebaeb8")
 label.place(x=70,y=0)
+
+def button_clicked():
+    def helloCallBack():
+       msg  = messagebox.showinfo( "arbeltry", "good")
+
+
+    B = Button(window, text ="Hello", command = helloCallBack, activebackground="blue", width=15, height=2)
+    B.place(x=200,y=200)
+
+def text_box_clicked():
+    my_entry = Entry(window, width=40)
+    my_entry.place(x=300,y=300)
+    my_entry.pack(pady=20)
+
+
+button_clicked()
 window.mainloop()
