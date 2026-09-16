3b02c1c05c4f575e1ea7863ec6ba0e838ed9475e
97252 <shilatkolani90@gmail.com>
2026-09-16 14:27:34 +0300

    new

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+global PSYCHOLOGIST
 PSYCHOLOGIST = [{"name":"noga","place":"south",'date':'15.9.26',"hour":"19:00","sub":"mother"},
                 {"name":"arbel","place":"north",'date':'17.10.26',"hour":"17:00","sub":"single mother"},
                 {"name":"yaheli","place":"north",'date':'20.9.26',"hour":"12:00","sub":"mother"},
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import os
 import filter
+import constants
 
 # Lucida Handwriting
 # Centaur
@@ -16,7 +17,6 @@ class Page1(Page):
        Page.__init__(self, *args, **kwargs)
        label = tk.Label(self, text="Welcome to our site",font=('Lucida Handwriting',20,'bold'),background="pink")
        label.pack(side="top")
-
        self.config(bg="pink")
 
 class Page2(Page):
@@ -25,16 +25,27 @@ class Page2(Page):
        self.config(bg="pink")
        name_var = tk.StringVar()
        passw_var = tk.StringVar()
+       Pname_ver = tk.StringVar()
+       loction_var = tk.StringVar()
+       date_var = tk.StringVar()
+
        #
        def submit():
            name = name_var.get()
            password = passw_var.get()
+           Pname = Pname_ver.get()
+           loction = loction_var.get()
+           date = date_var.get()
 
-           print("The name is : " + name)
-           print("The password is : " + password)
+           dict_temp = {"name": Pname, "place": loction, 'date': date, "hour": password, "sub": name}
+           constants.PSYCHOLOGIST.append(dict_temp)
+           print(constants.PSYCHOLOGIST)
 
            name_var.set("")
            passw_var.set("")
+           Pname_ver.set("")
+           loction_var.set("")
+           date_var.set("")
 
        name_label = tk.Label(self, text='class theme', font=('calibre', 10, 'bold'))
        name_label.pack(side="top")
@@ -44,17 +55,40 @@ class Page2(Page):
        passw_label.pack(side="top")
        passw_entry = tk.Entry(self, textvariable=passw_var, font=('calibre', 10, 'normal'))
        passw_entry.pack(side="top")
+       loc_label = tk.Label(self, text='loction', font=('calibre', 10, 'bold'))
+       loc_label.pack(side="top")
+       loc_entry = tk.Entry(self, textvariable=loction_var, font=('calibre', 10, 'normal'))
+       loc_entry.pack(side="top")
+       date_label = tk.Label(self, text='date', font=('calibre', 10, 'bold'))
+       date_label.pack(side="top")
+       date_entry = tk.Entry(self, textvariable=date_var, font=('calibre', 10, 'normal'))
+       date_entry.pack(side="top")
+       Pname_label = tk.Label(self, text='your name', font=('calibre', 10, 'bold'))
+       Pname_label.pack(side="top")
+       Pname_entry = tk.Entry(self, textvariable=Pname_ver, font=('calibre', 10, 'normal'))
+       Pname_entry.pack(side="top")
        sub_btn = tk.Button(self, text='Submit', command=submit)
        sub_btn.pack(side="top")
-
 
 
 class Page3(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
        self.config(bg="pink")
-       list_of_loc={}
-       list_of_loc=filter.filter1(self)
+       filter.filter1(self)
+
+       def text_list():
+           temp_text = ""
+           for i in range(len(constants.PSYCHOLOGIST)):
+               for key in constants.PSYCHOLOGIST[i].keys():
+                   temp_text += constants.PSYCHOLOGIST[i][key] + " "
+               temp_text += "\n"
+           print(temp_text)
+           return temp_text
+
+       text1 = text_list()
+       meet = tk.Label(self, text=text1, font=('calibre', 10, 'bold'), background="pink")
+       meet.pack(side="top")
 
 
 class MainView(tk.Frame):
