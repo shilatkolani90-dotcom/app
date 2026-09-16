dd681de2cfaa7bbb5dace52b88ba2f310736a819
97252 <shilatkolani90@gmail.com>
2026-09-16 09:29:43 +0300

    new

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Page1(Page):
        label.pack(side="top")
 
        self.config(bg="pink")
-
+#
 class Page2(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
