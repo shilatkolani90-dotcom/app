472369ef7e9186ab993c2a41551cceac4555d4be
studentskv402 <yahelihalperin@gmail.com>
2026-09-16 12:31:20 +0300

    jhj

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-PSYCHOLOGIST = [{"name":"noga","place":"south",'date':'15.9.26',"day":"monday","hour":"19:00"},
-                {"name":"arbel","place":"north",'date':'17.10.26',"day":"sunday","hour":"17:00"},
-                {"name":"yaheli","place":"north",'date':'20.9.26',"day":"wednesday","hour":"12:00"},
-                {"name":"shilat","place":"center",'date':'25.9.26',"day":"thursday","hour":"18:30"}]
+PSYCHOLOGIST = [{"name":"noga","place":"south",'date':'15.9.26',"hour":"19:00","sub":"mother"},
+                {"name":"arbel","place":"north",'date':'17.10.26',"hour":"17:00","sub":"single mother"},
+                {"name":"yaheli","place":"north",'date':'20.9.26',"hour":"12:00","sub":"mother"},
+                {"name":"shilat","place":"center",'date':'25.9.26',"hour":"18:30","sub":"mother"}]
 ##
