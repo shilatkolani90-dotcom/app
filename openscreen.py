76ff78157dcce09d273e0680fb3b135b7d3f9126
arbelz08 <arbelz08@gmail.com>
2026-09-16 10:06:28 +0300

    arbel

--- openscreen.py
+++ openscreen.py
@@ -14,19 +14,39 @@ photo=PhotoImage(file='mom.png')
 label=Label(window,text="hello,welcome to our site",font=('Flux',40,'bold'),fg="white",background="#ebaeb8")
 label.place(x=70,y=0)
 
-def button_clicked():
-    def helloCallBack():
-       msg  = messagebox.showinfo( "arbeltry", "good")
+# def button_clicked():
+#     def helloCallBack():
+#        msg  = messagebox.showinfo( "arbeltry", "good")
+#
+#
+#     B = Button(window, text ="Hello", command = helloCallBack, activebackground="blue", width=15, height=2)
+#     B.place(x=200,y=200)
+
+# def text_box_clicked():
+#     my_entry = Entry(window, width=40)
+#     my_entry.place(x=300,y=300)
+#     my_entry.pack(pady=20)
+
+import tkinter as tk
+from tkinter import PhotoImage
+
+#
+# # Load the image
+# image = PhotoImage(file="doctor.png")
+#
+# # Create a label to display the image
+# image_label = tk.Label(window, image=image)
+# image_label.pack()
+
+# path = tk.filedialog.askopenfilename(filetypes=fileTypes)
+# img = Image.open(path)
+# img = img.resize((200, 200))
+# pic = ImageTk.PhotoImage(img)
+#
+# app.geometry("560x300")
+# label.config(image=pic)
+# label.image = pic
 
 
-    B = Button(window, text ="Hello", command = helloCallBack, activebackground="blue", width=15, height=2)
-    B.place(x=200,y=200)
 
-def text_box_clicked():
-    my_entry = Entry(window, width=40)
-    my_entry.place(x=300,y=300)
-    my_entry.pack(pady=20)
-# a
-
-button_clicked()
 window.mainloop()
