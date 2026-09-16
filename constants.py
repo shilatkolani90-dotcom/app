ab285a7cbe7fea595d2ffca18c2870e62fc1d6af
studentskv402 <yahelihalperin@gmail.com>
2026-09-16 12:16:44 +0300

    ghgh

---
+++ constants.py
@@ -0,0 +1,4 @@
+PSYCHOLOGIST = [{"name":"noga","place":"south",'date':'15.9.26',"day":"monday","hour":"19:00"},
+                {"name":"arbel","place":"north",'date':'17.10.26',"day":"sunday","hour":"17:00"},
+                {"name":"yaheli","place":"north",'date':'20.9.26',"day":"wednesday","hour":"12:00"},
+                {"name":"shilat","place":"center",'date':'25.9.26',"day":"thursday","hour":"18:30"}]
